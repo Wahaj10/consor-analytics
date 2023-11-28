276e766a14903bc3df58f08463e74b77f9e7bc4d
Wahaj Taseer <wahajtaseer@Wahajs-MacBook-Pro.local>
2023-11-27 20:22:23 -0700

    About page updates with FAQ

--- src/demos/AboutPage.js
+++ src/demos/AboutPage.js
@@ -1,39 +1,18 @@
 import React from "react";
-import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-// import Hero from "components/hero/TwoColumnWithVideo.js";
-import Hero from "components/hero/TwoColumnWithVideo.js";
+import Header from "../components/headers/light.js";
+
 import AboutAreaOne from "demos/AboutAreaOne.js";
 import FaqAreaOne from "demos/FaqAreaOne.js";
-// import MainFeature from "components/features/TwoColWithButton.js";
-// import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
-// import DownloadApp from "components/cta/DownloadApp.js";
-// import MainFeature from "components/features/TwoColSingleFeatureWithStats.js";
 
 import Footer from "components/footers/FiveColumnWithInputForm.js";
 
 export default () => {
-  // const Subheading = tw.span`tracking-wider text-sm font-medium`;
-  // const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
-  // const Description = tw.span`inline-block mt-8`;
-  const imageCss = tw`rounded-4xl`;
   return (
     <AnimationRevealPage>
-      <Hero
-        heading={
-          <>
-            Data-driven Insights for Your {"  "}
-            <span tw="text-primary-500">Success</span>
-          </>
-        }
-        description="Insights derived from comprehensive data analysis."
-        imageSrc="https://lpsonline.sas.upenn.edu/sites/default/files/2022-10/plpso-feratures-data-business.jpg"
-        imageCss={imageCss}
-        imageDecoratorBlob={true}
-        primaryButtonText="Get in Touch"
-        watchVideoButtonText="Why is Big Data Important"
-      />
+      <Header />
+
       <AboutAreaOne />
       <FaqAreaOne />
 
--- src/demos/FaqAreaOne.js
+++ src/demos/FaqAreaOne.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-// import img1 from "assets/img/about/4.png";
-// import img2 from "assets/img/about/5.png";
 import img3 from "assets/img/bg/3.png";
 
 const FaqAreaOne = () => {
@@ -13,7 +11,7 @@ const FaqAreaOne = () => {
         style={{ backgroundImage: `url(${img3})` }}
       >
         <div className="container">
-          <div className="row pd-top-120">
+          <div className="row ">
             <div
               className="col-xl-5 col-lg-6 col-md-8 order-lg-last"
               data-aos="fade-left"
@@ -46,11 +44,11 @@ const FaqAreaOne = () => {
                 <h2 className="title">
                   Creating <span>Digital</span> Masterpieces
                 </h2>
-                <p className="content">
+                {/* <p className="content">
                   It is a long established fact that a reader will be distr
                   acted bioiiy the end gail designa readable content of a page
                   when looking.
-                </p>
+                </p> */}
               </div>
               <div
                 className="accordion accordion-inner style-2 accordion-icon-left mt-3"
@@ -66,7 +64,7 @@ const FaqAreaOne = () => {
                       aria-expanded="true"
                       aria-controls="collapseOne"
                     >
-                      What services do you offer?
+                      What services does your data analytics company offer?
                     </button>
                   </h2>
                   <div
@@ -76,9 +74,10 @@ const FaqAreaOne = () => {
                     data-bs-parent="#accordionExample"
                   >
                     <div className="accordion-body">
-                      Many desktop publishing packages and web page editors now
-                      use Lorem Ipsum as their default model text, search for
-                      'lorem ipsum' will uncover
+                      We offer a comprehensive range of data analytics services,
+                      including data interpretation, predictive modeling,
+                      machine learning, and customized solutions to meet your
+                      specific business needs.
                     </div>
                   </div>
                 </div>
@@ -92,7 +91,7 @@ const FaqAreaOne = () => {
                       aria-expanded="false"
                       aria-controls="collapseTwo"
                     >
-                      How long does it take for you to complete a project?
+                      How can data analytics benefit my business?
                     </button>
                   </h2>
                   <div
@@ -102,9 +101,10 @@ const FaqAreaOne = () => {
                     data-bs-parent="#accordionExample"
                   >
                     <div className="accordion-body">
-                      Many desktop publishing packages and web page editors now
-                      use Lorem Ipsum as their default model text, search for
-                      'lorem ipsum' will uncover
+                      Data analytics can provide valuable insights into your
+                      business operations, customer behavior, and market trends,
+                      helping you make informed decisions, optimize processes,
+                      and gain a competitive edge.
                     </div>
                   </div>
                 </div>
@@ -118,7 +118,7 @@ const FaqAreaOne = () => {
                       aria-expanded="false"
                       aria-controls="collapseThree"
                     >
-                      How much does it cost to work with your agency?
+                      What industries do you specialize in?
                     </button>
                   </h2>
                   <div
@@ -128,9 +128,146 @@ const FaqAreaOne = () => {
                     data-bs-parent="#accordionExample"
                   >
                     <div className="accordion-body">
-                      Many desktop publishing packages and web page editors now
-                      use Lorem Ipsum as their default model text, search for
-                      'lorem ipsum' will uncover
+                      Our expertise spans across various industries, including
+                      but not limited to finance, healthcare, retail, and
+                      manufacturing. We tailor our solutions to suit the unique
+                      challenges of each sector.
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingFour">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseFour"
+                      aria-expanded="false"
+                      aria-controls="collapseFour"
+                    >
+                      How do you ensure the security and confidentiality of our
+                      data?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseFour"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingFour"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      Security is paramount to us. We employ state-of-the-art
+                      encryption, access controls, and other measures to
+                      safeguard your data. Our practices comply with industry
+                      standards and regulations.
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingFive">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseFive"
+                      aria-expanded="false"
+                      aria-controls="collapseFive"
+                    >
+                      Can you integrate with existing systems and platforms?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseFive"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingFive"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      Absolutely. Our team is experienced in seamless
+                      integrations with various systems and platforms to ensure
+                      a smooth transition and continued compatibility.
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingSix">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseSix"
+                      aria-expanded="false"
+                      aria-controls="collapseSix"
+                    >
+                      How long does it take to see results from data analytics?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseSix"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingSix"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      The timeline for results varies based on the complexity of
+                      your project. We work efficiently to deliver actionable
+                      insights promptly, but the duration can depend on the
+                      scope and objectives of the analysis.
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingSeven">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseSeven"
+                      aria-expanded="false"
+                      aria-controls="collapseSeven"
+                    >
+                      Do I need to have a large amount of data for analytics to
+                      be effective?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseSeven"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingSeven"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      Not necessarily. While more data can enhance the depth of
+                      insights, we can work with varying data volumes. Our team
+                      can assess your specific needs and tailor solutions
+                      accordingly.
+                    </div>
+                  </div>
+                </div>
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingEight">
+                    <button
+                      className="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseEight"
+                      aria-expanded="false"
+                      aria-controls="collapseEight"
+                    >
+                      What level of support do you provide after implementation?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseEight"
+                    className="accordion-collapse collapse"
+                    aria-labelledby="headingEight"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div className="accordion-body">
+                      Our support extends beyond implementation. We offer
+                      ongoing assistance, training, and regular reviews to
+                      ensure the continued success of our solutions and adapt
+                      them to your evolving needs.
                     </div>
                   </div>
                 </div>
