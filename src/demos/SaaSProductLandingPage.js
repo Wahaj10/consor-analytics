f287d5334d96723579ee6e91aea4577795a70475
Wahaj Taseer <wahajtaseer@Wahajs-MacBook-Pro.local>
2023-11-27 20:39:35 -0700

    example tstimonials

--- src/demos/SaaSProductLandingPage.js
+++ src/demos/SaaSProductLandingPage.js
@@ -4,6 +4,11 @@ import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import Hero from "components/hero/TwoColumnWithInput.js";
+import Testimonials from "components/testimonials/SimplePrimaryBackground.js";
+import Testimonials2 from "components/testimonials/ThreeColumnWithProfileImage.js";
+import Testimonials3 from "components/testimonials/TwoColumnWithImage.js";
+import Testimonials4 from "components/testimonials/TwoColumnWithImageAndProfilePictureReview.js";
+import Testimonials5 from "components/testimonials/TwoColumnWithImageAndRating.js";
 import ServiceAreaTwo from "./ServiceAreaTwo.js";
 import CounterAreaFour from "./CounterAreaFour.js";
 import FeatureWithSteps from "components/features/TwoColWithSteps.js";
@@ -37,6 +42,12 @@ export default () => {
         decoratorBlobCss={tw`xl:w-40 xl:h-40 opacity-15 -translate-x-1/2 left-1/2`}
       />
       <BrandAreaOne />
+
+      <Testimonials></Testimonials>
+      <Testimonials2></Testimonials2>
+      <Testimonials3></Testimonials3>
+      <Testimonials4></Testimonials4>
+      <Testimonials5></Testimonials5>
       <Footer />
     </AnimationRevealPage>
   );
