dcfac9c0c54874fae9305c9b29f1c998ea86a3d1
Wahaj Taseer <wahaj.taseer@gmail.com>
2023-12-28 10:53:08 -0700

    Minor updates to services page

--- src/scripts/serviceList.js
+++ src/scripts/serviceList.js
@@ -2,35 +2,35 @@ let serviceList = [
   {
     title: "Data Integration & Ingestion",
     des:
-      "Bridging the gaps in your data universe,offering a harmonised integration of both structured and" +
+      "Bridging the gaps in your data universe,offering a harmonized integration of both structured and " +
       "unstructured data sources, ensuring a cohesive dataset ready for analysis.",
     img: "assets/img/service-icon/1.png",
   },
   {
     title: "Data Cleansing and Quality Assurance",
     des:
-      "Achieve data readiness for analytics with precision. Utilising industry-leading platforms and" +
+      "Achieve data readiness for analytics with precision. Utilising industry-leading platforms and " +
       "tools, we meticulously cleanse, enrich, and profile your data, enhancing its quality and preparing it for insightful extraction.",
     img: "assets/img/service-icon/2.png",
   },
   {
     title: "Comprehensive Analytics Suite",
     des:
-      "Transform your business with our Comprehensive Analytics Suite. It combines Predictive," +
+      "Transform your business with our Comprehensive Analytics Suite. It combines Predictive, " +
       "Diagnostic, and Descriptive Analytics to deliver unparalleled insights, empowering you to make data-backed decisions that drive success.",
     img: "assets/img/service-icon/3.png",
   },
   {
     title: "Data Visualisation",
     des:
-      "Witness your data come to life. Leveraging vivid dashboards and a plethora of visualisation tools," +
+      "Witness your data come to life. Leveraging vivid dashboards and a plethora of visualisation tools, " +
       "we craft intuitive narratives from complex data matrices.",
     img: "assets/img/service-icon/2.png",
   },
   {
     title: "Data Strategy Development",
     des:
-      "Actualize your data's potential. In close collaboration with your team, we outline objectives," +
+      "Actualize your data's potential. In close collaboration with your team, we outline objectives, " +
       "curate technological solutions, and draft a blueprint to capitalise on data-led insights, refining your competitive stance.",
     img: "assets/img/service-icon/1.png",
   },
@@ -38,7 +38,7 @@ let serviceList = [
   {
     title: "Comprehensive Training and Workshops",
     des:
-      "Equip your team for the data-driven era. Our tailored training modules," +
+      "Equip your team for the data-driven era. Our tailored training modules, " +
       "ranging from foundational data literacy to analytics tool mastery, empower your workforce to optimally leverage data assets.",
     img: "assets/img/service-icon/3.png",
   },
