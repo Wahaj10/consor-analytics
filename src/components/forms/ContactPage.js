b510a19d20fb911be1a927dd376246635353c012
Wahaj Taseer <wahaj.taseer@gmail.com>
2024-01-27 20:44:34 -0700

    error fix

--- src/components/forms/ContactPage.js
+++ src/components/forms/ContactPage.js
@@ -5,7 +5,7 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 import // SectionHeading,
 // Subheading as SubheadingBase,
 "components/misc/Headings.js";
-import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
+// import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
 // import EmailIllustrationSrc from "images/email-illustration.svg";
 import contactImg from "images/ContactUs.png";
 
@@ -36,12 +36,13 @@ const TextContent = tw.div`lg:py-8 text-center md:text-left`;
 const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100`;
 
 // const Form = tw.form`mt-8 md:mt-10 text-sm flex flex-col max-w-sm mx-auto md:mx-0`;
-const Input = tw.input`mt-6 first:mt-0 border-b-2 py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500`;
-const Textarea = styled(Input).attrs({ as: "textarea" })`
-  ${tw`h-24`}
-`;
 
-const SubmitButton = tw(PrimaryButtonBase)`inline-block mt-8`;
+// const Input = tw.input`mt-6 first:mt-0 border-b-2 py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500`;
+// const Textarea = styled(Input).attrs({ as: "textarea" })`
+//   ${tw`h-24`}
+// `;
+
+// const SubmitButton = tw(PrimaryButtonBase)`inline-block mt-8`;
 
 export default ({
   subheading = "Contact Us",
