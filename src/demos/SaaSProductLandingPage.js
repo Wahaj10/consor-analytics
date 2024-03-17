e12b35833e864ec3c1df85d55d446c8f2cd50beb
Wahaj Taseer <wahaj.taseer@gmail.com>
2024-03-17 13:25:18 -0600

    Removed testimonials

--- src/demos/SaaSProductLandingPage.js
+++ src/demos/SaaSProductLandingPage.js
@@ -36,7 +36,7 @@ export default () => {
         imageDecoratorBlob={true}
         decoratorBlobCss={tw`xl:w-40 xl:h-40 opacity-15 -translate-x-1/2 left-1/2`}
       />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <BrandAreaOne />
 
       <Footer />
